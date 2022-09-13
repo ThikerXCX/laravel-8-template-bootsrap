f48869b8c287a89cbd6126f81324f7fea508f88e
ThikerXCX <rifkimuhammad967@gmail.com>
2022-09-13 19:56:42 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
+
+    <div class="row">
+        <col-md-6>
+            <div class="card">
+                <div class="card-body">
+                    <h1>test</h1>
+                </div>
+            </div>
+        </col-md-6>
+    </div>
+</x-app-layout>
